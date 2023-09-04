03d44659a7783e8dd0cb6f27dfb41352da3d804a
FoursClub <84503482+FoursClub@users.noreply.github.com>
2023-09-04 13:33:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Footer, Blog, Features, Possibillity, Header, WhatGPT3 } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+const App = () => {
+  return (
+    <>
+      <div className='App'>
+        <div className='gradiant_bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibillity />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
